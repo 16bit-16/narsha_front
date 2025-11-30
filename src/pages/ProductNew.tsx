f94f241bae05f57943f1ffcb1f63c250d566a0db
16bit-16 <lhy93380@gmail.com>
2025-12-01 08:43:25 +0900

    fix: vercel deploy 디버그

--- src/pages/ProductNew.tsx
+++ src/pages/ProductNew.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import { unstable_setDevServerHooks, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Map from "../components/Map";
 
 import { api } from "../utils/api";
